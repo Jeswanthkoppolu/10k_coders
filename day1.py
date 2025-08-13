0de9511dfa2ddbfe9d9263e5a8faef708183d1b4
jeswanth <jeswanthkoppolu@gmail.com>
2025-08-14 02:18:56 +0530

    Moved all Python files into 'python' folder

--- day1.py
+++
@@ -1,9 +0,0 @@
-name=input("enter your name:")
-city=input("eter your city:")
-branch=input("enter your branch:")
-college=input("enter your college name:")
-
-print(f"my name is {name}.i live in {city}. i completed my btech in {branch}from{college}.")
-
-
-
